7463d34ca4aa9c9c3a64a5d0bdde8b72f0530833
kyle_rosenmeyer <kylerosenmeyer@gmail.com>
2018-11-14 23:23:57 -0500

    animation update

--- app/public/home.js
+++ app/public/home.js
@@ -1,9 +1,37 @@
-TweenMax.to(".landing-page", 2.0, 
-    {
-        transformOrigin: "100% 100%",
-        ease: Bounce.easeOut, 
-        yPercent: 50,
-        skewY: -2,
-        xPercent: -1.0
-    }
-)
+
+if ( window.innerWidth > 1000 ) {
+
+    TweenMax.to(".landing-page", 2.0, 
+        {
+            transformOrigin: "100% 100%",
+            ease: Bounce.easeOut, 
+            yPercent: 50,
+            skewY: -2,
+            xPercent: -1.0
+        }
+    )   
+} else if ( window.innerWidth > 650 ) {
+
+    TweenMax.to(".landing-page", 2.0, 
+        {
+            transformOrigin: "100% 100%",
+            ease: Bounce.easeOut, 
+            yPercent: 30,
+            skewY: -2,
+            xPercent: -1.0
+        }
+    ) 
+} else {
+
+    TweenMax.to(".landing-page", 2.0, 
+        {
+            transformOrigin: "100% 100%",
+            ease: Bounce.easeOut, 
+            yPercent: 10,
+            skewY: -2,
+            xPercent: -1.0
+        }
+    ) 
+}
+
+
